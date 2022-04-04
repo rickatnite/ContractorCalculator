996f7434e1a16aac34b7c29028a59fa9cde1db8f
ric <eroche1@student.gsu.edu>
2022-04-04 18:11:53 -0400

    Initial Commit

---
+++ ContractorCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContractorCalculator
+//
+//  Created by E Roche on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
