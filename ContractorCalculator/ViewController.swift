892bd42ada15812145c9b38f4e50d093329bc7ba
ric <eroche1@student.gsu.edu>
2022-04-06 01:34:10 -0400

    added functionality - need to format output

--- ContractorCalculator/ViewController.swift
+++ ContractorCalculator/ViewController.swift
@@ -6,9 +6,38 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calcBtnOutlet: UIButton!
+    @IBOutlet weak var txtLabor: UITextField!
+    @IBOutlet weak var txtMaterials: UITextField!
+    @IBOutlet weak var lblSubtotal: UILabel!
+    @IBOutlet weak var lblTax: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+        var labor:Double? = nil
+        labor = Double(txtLabor.text!)
+        var materials:Double? = nil
+        materials = Double(txtMaterials.text!)
+        
+        if labor != nil && materials != nil {
+            let sub = labor! + materials!
+            let tax = sub * 0.05
+            let total = sub + tax
+            let subTxt = "$\(sub)"
+            let taxTxt = "$\(tax)"
+            let totTxt = "$\(total)"
+            lblSubtotal.text = subTxt
+            lblTax.text = taxTxt
+            lblTotal.text = totTxt
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
